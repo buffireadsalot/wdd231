3cbb0952843673a7ea61d2c8980df6fd7939fc39
buffireadsalot <annelabuffin@gmail.com>
2025-09-06 13:12:36 -0400

    final updates

--- scripts/course.js
+++ scripts/course.js
@@ -1,10 +1,10 @@
 // Course data (you can flip completed: true on the ones you've finished)
 const courses = [
-    { subject: 'CSE', number: 110, title: 'Introduction to Programming', credits: 2, certificate: 'Web and Computer Programming', technology: ['Python'], completed: false },
-    { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 2, certificate: 'Web and Computer Programming', technology: ['HTML', 'CSS'], completed: false },
-    { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 2, certificate: 'Web and Computer Programming', technology: ['Python'], completed: false },
-    { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 2, certificate: 'Web and Computer Programming', technology: ['C#'], completed: false },
-    { subject: 'WDD', number: 131, title: 'Dynamic Web Fundamentals', credits: 2, certificate: 'Web and Computer Programming', technology: ['HTML', 'CSS', 'JavaScript'], completed: false },
+    { subject: 'CSE', number: 110, title: 'Introduction to Programming', credits: 2, certificate: 'Web and Computer Programming', technology: ['Python'], completed: true },
+    { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 2, certificate: 'Web and Computer Programming', technology: ['HTML', 'CSS'], completed: true },
+    { subject: 'CSE', number: 111, title: 'Programming with Functions', credits: 2, certificate: 'Web and Computer Programming', technology: ['Python'], completed: true },
+    { subject: 'CSE', number: 210, title: 'Programming with Classes', credits: 2, certificate: 'Web and Computer Programming', technology: ['C#'], completed: true },
+    { subject: 'WDD', number: 131, title: 'Dynamic Web Fundamentals', credits: 2, certificate: 'Web and Computer Programming', technology: ['HTML', 'CSS', 'JavaScript'], completed: true },
     { subject: 'WDD', number: 231, title: 'Frontend Web Development I', credits: 2, certificate: 'Web and Computer Programming', technology: ['HTML', 'CSS', 'JavaScript'], completed: false }
 ];
 
